349daef5bc5054eac1124c1c59558bc67ad6b748
Gaurav Khatri <khatrigaurav09@gmail.com>
2025-07-15 20:34:36 +0530

    project completed

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,12 @@ import cookieParser from 'cookie-parser';
 import cors from "cors";
 import { app, server } from './src/config/socket.js';
 
+import { path } from "path";
+
 dotenv.config();
 const PORT = process.env.PORT
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' })); //this will allow us to extract json data out of the body
 app.use(cookieParser()); //this will allowed you to parse the cookies
@@ -21,6 +25,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 
 
